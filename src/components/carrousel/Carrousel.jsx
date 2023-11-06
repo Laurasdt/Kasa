e6716dfceeeb863a860d765eb8b3b102620aee36
Laura Schmidt <lauramarie.sdt@gmail.com>
2023-11-06 17:28:15 +0100

    title + location logements

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -1,13 +1,52 @@
-import './carrousel.scss'
+import './carrousel.scss';
+import { useState } from 'react';
+import ArrowForward from '../../Assets/ArrowForward.png'
+import ArrowBack from '../../Assets/ArrowForward.png'
 
-const Carrousel = () => {
-  
-    return (
-      <section className='Carroussel'>
-       
-      </section>
-    );
+const Carrousel = ({ slides }) => {
+  const slidesLength = slides.length;
+  const [place, setPlace] = useState(0);
+  const showArrows = slidesLength > 1;
 
-} ;
+  const SlideBack = () => {
+    setPlace(place === 0 ? slidesLength - 1 : place - 1);
+  };
+
+  const SlideForward = () => {
+    setPlace(place === slidesLength - 1 ? 0 : place + 1);
+  };
+
+  return (
+    <section className='Carrousel'>
+      {slides.map((slide, index) => (
+        <div key={index} className="Carrousel__container">
+          {index === place && (
+            <div>
+              <img className='Slide'src={slide} alt="Photo du logement"/>
+              {showArrows && (
+                <>
+             
+                  <img
+                    className='ArrowBack'
+                    src={ArrowBack}
+                    alt='flèche back'
+                    onClick={SlideBack}
+                  />
+                  <img
+                    className='ArrowForward'
+                    src={ArrowForward}
+                    alt="flèche next"
+                    onClick={SlideForward}
+                  />
+                
+                </>
+                )}
+           </div>
+          )}
+        </div>
+      ))}
+    </section>
+  );
+};
 
 export default Carrousel;
--- src/pages/logement/Logement.jsx
+++ src/pages/logement/Logement.jsx
@@ -1,15 +1,28 @@
 import './logement.scss'
 import Carrousel from '../../components/carrousel/Carrousel'
 import { useParams } from 'react-router-dom';
+import data from '../../data/logements.json';
+import NotFound from '../notFound/NotFound'
 
-function LogementPage() {
+function LogementPage(props) {
+
   const { id } = useParams();
   console.log(id);
+
+  const logement = data.find(logement => logement.id === id);
+  const title = logement ? logement.title : "";
+  const location = logement ? logement.location : "";
+
+  if(logement === undefined){
+    return <NotFound />
+}
   return (
     <section className='LogementPage'>
 
-        <Carrousel />
-
+        <Carrousel slides={logement.pictures} />
+        <h1>{title}</h1>
+        <h2>{location}</h2>
+    
     </section>
   )
 };
